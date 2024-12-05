1ba6bca8ce491809aa05ec6a6d4227953ef641da
Lucas-VTL <votanloc1106@gmail.com>
2024-12-05 21:56:30 +0700

    Upload resources

---
+++ Assets/Scripts/PlayerArmController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerArmController : MonoBehaviour
+{
+    public GameObject player;
+    
+    private Camera _mainCamera;
+    private float _yOffset = 0.6f;
+    
+    void Start()
+    {
+        _mainCamera = Camera.main;
+    }
+    
+    void Update()
+    {
+        var mousePos = Input.mousePosition;
+        mousePos = _mainCamera.ScreenToWorldPoint(mousePos);
+        mousePos.z = 0;
+        
+        Vector3 direction = (mousePos - transform.position).normalized;
+        var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        angle = Mathf.Repeat(angle, 360f);
+
+        if (angle <= 60 || angle >= 300)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            player.transform.localScale = new Vector3(1, 1, 1);
+            
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+        else if (angle >= 120 && angle <= 240)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+            player.transform.localScale = new Vector3(-1, 1, 1);
+            
+            transform.rotation = Quaternion.Euler(0, 0, angle + 180);
+        }
+        
+        transform.position = player.transform.position + new Vector3(0, _yOffset, 0);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Animator _animator;
+    private int _playerSpeed;
+    
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+    
+    void Update()
+    {
+        var horizontalMove = Input.GetAxisRaw("Horizontal");
+        var verticalMove = Input.GetAxisRaw("Vertical");
+        var move = new Vector3(horizontalMove, verticalMove, 0);
+
+        if (horizontalMove == 0 && verticalMove == 0)
+        {
+            _animator.SetBool("isWalking", false);
+            _animator.SetBool("isRunning", false);
+        }
+        else
+        {
+            if (transform.localScale.x * horizontalMove > 0)
+            {
+                _animator.SetBool("isStepBack", false);
+            }
+            else
+            {
+                _animator.SetBool("isStepBack", true);
+            }
+            
+            _animator.SetBool("isWalking", true);
+            
+            if (Input.GetKey(KeyCode.Space))
+            {
+                _animator.SetBool("isRunning", true);
+                _playerSpeed = 5;
+            }
+            else
+            {
+                _animator.SetBool("isRunning", false);
+                _playerSpeed = 3;
+            }
+            
+            transform.position += move * (_playerSpeed * Time.deltaTime);
+        }
+    }
+}
